66177cea78545384792a52e52a3fd796bda9cd85
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-10-21 14:32:13 +0200

    Autenticación básica en api monumentos

--- ApiMonumentosBasic/src/main/java/com/salesianostriana/dam/rest/seguridad/SpringSecurityConfig.java
+++ ApiMonumentosBasic/src/main/java/com/salesianostriana/dam/rest/seguridad/SpringSecurityConfig.java
@@ -2,15 +2,13 @@ package com.salesianostriana.dam.rest.seguridad;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
-@EnableWebSecurity
-
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -19,17 +17,25 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.csrf().disable()
+		http.
+			httpBasic()
+			.authenticationEntryPoint(authenticationEntryPoint)	
+			.and()
 			.authorizeRequests()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-			.authenticationEntryPoint(authenticationEntryPoint);	
+			.antMatchers(HttpMethod.GET, "/monumentos/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers(HttpMethod.POST, "/monumento/").hasRole("ADMIN")
+			.antMatchers(HttpMethod.PUT, "/monumentos/**").hasRole("ADMIN")
+			.antMatchers(HttpMethod.DELETE, "/monumentos/**").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.csrf().disable();
+			
 	}
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN")
+			.and().withUser("user").password("{noop}user").roles("USER");
 	}
 	
 
